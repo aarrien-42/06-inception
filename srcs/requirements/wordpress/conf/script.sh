dd7a3ed4de9df03b81cdce3c47b3fb162b172eaa
aarrien- <andoniarrien27@gmail.com>
2023-11-08 15:16:37 +0100

    Wordpress working but admin menu still not fully working

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -5,15 +5,17 @@ done
 mkdir -p /var/www/html
 cd /var/www/html
 
+sed -i "s/database_name_here/${MYSQL_DATABASE}/g" /var/www/html/wp-config.php
+sed -i "s/username_here/${MYSQL_USER}/g" /var/www/html/wp-config.php
+sed -i "s/password_here/${MYSQL_PASSWORD}/g" /var/www/html/wp-config.php
+sed -i "s/localhost/${MYSQL_HOST}/g" /var/www/html/wp-config.php
+
 wp core download --allow-root
 
 wp config create --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --locale=es_ES --allow-root --skip-check
 
-wp core install --path=/var/www/html --url=$DOMAIN_NAME --title="Esto Funciona!!!" --admin_name=$WP_ADMIN_USER --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email="aarrien@student.42urduliz.com" --skip-email --allow-root
+wp core install --path=/var/www/html --url=localhost --title="Esto Funciona!!!" --admin_name=$WP_ADMIN_USER --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email="aarrien@student.42urduliz.com" --skip-email --allow-root
 
-#sed -i "s/database_name_here/${MYSQL_DATABASE}/g" /var/www/html/wp-config.php
-#sed -i "s/username_here/${MYSQL_USER}/g" /var/www/html/wp-config.php
-#sed -i "s/password_here/${MYSQL_PASSWORD}/g" /var/www/html/wp-config.php
-#sed -i "s/localhost/${MYSQL_HOST}/g" /var/www/html/wp-config.php
+wp theme activate "twentytwentytwo"
 
 php-fpm81 --nodaemonize
