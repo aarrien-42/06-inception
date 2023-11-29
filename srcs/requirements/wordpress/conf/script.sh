522d46d187c2b00dbad486a184a36476a58cf4e9
Andoni Arrien Martin <aarrien-@c4r8s4.42urduliz.com>
2023-11-29 19:56:22 +0100

    update

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -15,20 +15,13 @@ wp theme activate "twentytwentytwo"
 
 cat << EOF > /var/www/html/wp-content/plugins/akismet/.htaccess
 
-RewriteRule ^wp-admin$ http://aarrien.42.fr/wp-login.php [NC,L]
+RewriteRule ^wp-admin$ https://aarrien.42.fr/wp-login.php [NC,L]
 
 RewriteEngine On
-RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 RewriteBase /
 RewriteRule ^index\.php$ - [L]
-
-RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]
-
-RewriteCond %{REQUEST_FILENAME} -f [OR]
-RewriteCond %{REQUEST_FILENAME} -d
-RewriteRule ^ - [L]
-RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]
-RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
 RewriteRule . index.php [L]
 
 EOF
