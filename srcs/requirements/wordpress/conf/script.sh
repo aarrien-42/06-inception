ae750cdb043e228f73b6666f38c8d7d0c7562b97
Andoni Arrien Martin <aarrien-@c4r8s4.42urduliz.com>
2023-11-29 19:35:12 +0100

    update

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -13,15 +13,21 @@ wp core install --path=/var/www/html --url=localhost --title="INCEPTION" --admin
 
 wp theme activate "twentytwentytwo"
 
-cat << EOF >> /var/www/html/wp-content/plugins/akismet/.htaccess
+cat << EOF > /var/www/html/wp-content/plugins/akismet/.htaccess
 
 RewriteEngine On
 RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 RewriteBase /
 RewriteRule ^index\.php$ - [L]
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteRule . /index.php [L]
+
+RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]
+
+RewriteCond %{REQUEST_FILENAME} -f [OR]
+RewriteCond %{REQUEST_FILENAME} -d
+RewriteRule ^ - [L]
+RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]
+RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]
+RewriteRule . index.php [L]
 
 EOF
 
