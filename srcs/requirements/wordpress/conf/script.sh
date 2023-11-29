25ba61ead5f6da3cf6ca3d10d5ab6ca2a2fe00cc
Andoni Arrien Martin <aarrien-@c4r8s4.42urduliz.com>
2023-11-29 16:35:46 +0100

    update

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -2,7 +2,7 @@ while ! nc -z mariadb 3306; do
     sleep 1
 done
 
-mdkir -p /var/www/html
+mkdir -p /var/www/html
 
 #sed -i "s/database_name_here/${MYSQL_DATABASE}/g" /var/www/html/wp-config.php
 #sed -i "s/username_here/${MYSQL_USER}/g" /var/www/html/wp-config.php
@@ -10,8 +10,6 @@ mdkir -p /var/www/html
 #sed -i "s/localhost/${MYSQL_HOST}/g" /var/www/html/wp-config.php
 
 wp core download --allow-root
-
-sleep 10
 
 wp config create --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --locale=es_ES --allow-root --skip-check
 
