5a272a08b28a39a44ad2deffb5d0474c9bef5545
Andoni Arrien Martin <aarrien-@c4r6s6.42urduliz.com>
2023-12-04 15:17:26 +0100

    update

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -9,7 +9,7 @@ wp core download --allow-root
 
 wp config create --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --locale=es_ES --allow-root --skip-check
 
-wp core install --path=/var/www/html --url=localhost --title="INCEPTION" --admin_name=$WP_ADMIN_USER --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email="aarrien@student.42urduliz.com" --skip-email --allow-root
+wp core install --path=/var/www/html --url=$DOMAIN_NAME --title="INCEPTION" --admin_name=$WP_ADMIN_USER --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email="aarrien@student.42urduliz.com" --skip-email --allow-root
 
 wp theme activate "twentytwentytwo"
 
