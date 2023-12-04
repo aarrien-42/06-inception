bae686b1fda7867024df824c0bf92e0f26d235b3
Andoni Arrien Martin <aarrien-@c4r6s6.42urduliz.com>
2023-12-04 15:01:18 +0100

    update

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -15,14 +15,15 @@ wp theme activate "twentytwentytwo"
 
 cat << EOF > /var/www/html/wp-content/plugins/akismet/.htaccess
 
-RewriteRule ^wp-admin$ https://aarrien.42.fr/wp-login.php [NC,L]
-
+<IfModule mod_rewrite.c>
 RewriteEngine On
+RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 RewriteBase /
 RewriteRule ^index\.php$ - [L]
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME} !-d
-RewriteRule . index.php [L]
+RewriteRule . /index.php [L]
+</IfModule>
 
 EOF
 
